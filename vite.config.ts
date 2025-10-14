c3c385d8fdb669653b976c0c23a818f0f3a310ba
thanhlv87 <thanhlv@1school.edu.vn>
2025-10-14 14:31:47 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './', // Replace QLDA-NPSC with your repository name
+  root: '.',          // chỉ định gốc dự án là thư mục hiện tại
+  build: {
+    outDir: 'dist',   // đầu ra build
 })
